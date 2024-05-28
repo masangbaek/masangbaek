ec337bbde283009bc27be92431b30963b0e80e63
masangbaek <masangbaek@gmail.com>
2024-05-28 15:20:52 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -0,0 +1,10 @@
+#def solution(cipher, code):
+#     len(cipher)
+#    return answer
+def solution(cipher, code):
+    return cipher[code -1 :: code]
+
+#def solution(cipher, code):
+#    for i in range(code-1, len(chipher), code): 
+#        retrun 
+    
