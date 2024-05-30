1e15a880172ed3b2a5eaa2a2357f38b0afbe3e78
masangbaek <masangbaek@gmail.com>
2024-05-30 14:25:18 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,7 +1,3 @@
 def solution(n):
-    answer = []
-     #range(start, stop, step)
-        #range(0, 100, 2) [0, 2, 4, ]
-    for i in range(1, n+1, 2):
-        answer.append(i)
-    return answer
+    return [i for i in range(1, n+1, 2)]
+    
