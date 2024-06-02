87f7519c9910ca19dd25cba38dd8665010841f3f
masangbaek <masangbaek@gmail.com>
2024-06-02 22:51:56 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,16 +1,21 @@
 def solution(keyinput, board):
-    answer = [0, 0]
+    a, b = [0, 0]
     
     x = board[0] // 2
     y = board[1] // 2
     
     for i in keyinput :
-        if i == 'up' and answer[1] + 1 <= y :
-            answer[1] += 1
-        elif i == 'down' and answer[1] - 1 >= -y :
-            answer[1] -= 1
-        elif i == 'left' and answer[0] -1 >= -x :
-            answer[0] -= 1
-        elif i == 'right' and answer[0] +1 <= x :
-            answer[0] += 1
-    return answer
+        if i == 'up' :
+            if b < y :
+                b += 1
+        elif i == 'down' :
+            if b > -y :
+                b -= 1
+        elif i == 'left' :
+            if a > -x :
+                a -= 1
+        elif i == 'right' :
+            if a < x :
+                a += 1
+    
+    return a, b
