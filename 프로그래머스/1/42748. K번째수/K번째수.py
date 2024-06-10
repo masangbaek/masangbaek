afa81a642cf2e3622eea84873a994416c2d9bb8b
masangbaek <masangbaek@gmail.com>
2024-06-10 20:53:12 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,3 @@
-
-def solution(array, commands) : 
-    answer = []
-    for com in commands :
-        i, j, k = com
-        answer.append(sorted(array[i - 1 : j])[k - 1])
-    
-    return answer
+def solution(array, commands) :
+    x = list(map(lambda x : sorted(array[x[0] - 1 : x[1]])[x[2] - 1], commands))
+    return x 
