a5bd1c36a722309030898788e2bc2c6793ffda6b
masangbaek <masangbaek@gmail.com>
2024-06-22 15:31:54 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -3,7 +3,7 @@ dice1, dice2, dice3 = map(int, input().split())
 # print(dice1, dice2, dice3)
 
 if dice1 == dice2 == dice3:
-    print(10000 + dice1 * 1000)
+    print(10000 + (dice1 or dice2 or dice3) * 1000)
 
 elif dice1 == dice2 or dice1 == dice3 or dice2 == dice3:
     if dice1 == dice2:
@@ -13,6 +13,7 @@ elif dice1 == dice2 or dice1 == dice3 or dice2 == dice3:
     elif dice2 == dice3:
         print(1000 + (dice2) * 100)
 
-else:
+elif (dice1 != dice2) and (dice1 != dice3) and (dice2 != dice3):
     max_dice = max(dice1, dice2, dice3)
     print(max_dice * 100)
+    
