a77b7058f17fb1238f76d95cb3672438b4e43870
masangbaek <masangbaek@gmail.com>
2024-06-24 17:19:56 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,6 +1,5 @@
 def solution(numbers):
-    sorted_numbers = sorted(numbers, reverse=True)  # 리스트를 내림차순으로 정렬
-    num1 = sorted_numbers[0]  # 가장 큰 값
-    num2 = sorted_numbers[1]  # 그 다음 큰 값
-    return num1 * num2
+    numbers.sort()
+    # print(numbers)
+    return numbers[-1] * numbers[-2]
     
