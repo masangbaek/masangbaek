24c367aa05b452fa7ec815770541460a969371c6
masangbaek <masangbaek@gmail.com>
2024-06-27 20:30:16 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,10 +1,9 @@
 def solution(num_list):
-    odd = 0 
-    even = 0 
+    even = []
+    odd = []
     for i in num_list:
-        if i % 2 == 0:
-            even += 1
+        if i % 2 != 0:
+            odd.append(i)
         else:
-            odd += 1
-    return [even, odd]
-
+            even.append(i)
+    return [len(even), len(odd)]
