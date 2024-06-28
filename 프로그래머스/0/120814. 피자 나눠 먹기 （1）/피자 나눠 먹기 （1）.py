1733da786f6adde69b45191fba934a1b9e46815f
masangbaek <masangbaek@gmail.com>
2024-06-28 12:08:17 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -0,0 +1,32 @@
+def solution(n):
+    if 1<= n < 8:
+        return 1
+    elif 8<= n < 15:
+        return 2
+    elif 15<= n < 22:
+        return 3
+    elif 22<= n < 29:
+        return 4
+    elif 29<= n < 36:
+        return 5
+    elif 36<= n <= 42:
+        return 6
+    elif 42< n <= 49:
+        return 7
+    elif 49< n <= 56:
+        return 8
+    elif 56< n <= 63:
+        return 9
+    elif 63< n <= 70:
+        return 10 
+    elif 70< n <= 77:
+        return 11
+    elif 77< n <= 84:
+        return 12
+    elif 84< n <= 91:
+        return 13
+    elif 91< n <= 98:
+        return 14
+    elif 99<= n <= 100:
+        return 15
+    
