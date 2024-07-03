454f3179ac58456f128737cf02f9061ac24adfb7
Sangbaek <masangbaek@gmail.com>
2024-07-03 19:53:46 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,8 +1,6 @@
 def solution(n, numlist):
     answer = []
     for i in numlist:
-        # print(i)
         if i % n == 0:
             answer.append(i)
-            # print(i)
     return answer
