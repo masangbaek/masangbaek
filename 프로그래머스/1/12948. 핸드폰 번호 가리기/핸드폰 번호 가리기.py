03ae95a88315a2794363e3b9aa321e76e4d5aba1
Sangbaek <masangbaek@gmail.com>
2024-07-06 16:33:43 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,11 +1,27 @@
-def solution(phone_number): 
-    # 전화번호 뒷 4자리를 제외한 나머지 숫자는 *이 되어야함 
-    result = '' # 문자열로 반환
-    back_n = ''
-    # 앞번호 *로 만들기 
-    for i in range(len(phone_number) - len(phone_number[-4:])):
-        result += "*"
-        # 번호 뒷 4자리만 남기자
-    back_n += phone_number[-4:]
+# def solution(phone_number): 
+#     # 전화번호 뒷 4자리를 제외한 나머지 숫자는 *이 되어야함 
+#     result = '' # 문자열로 반환
+#     back_n = ''
+#     # 앞번호 *로 만들기 
+#     for i in range(len(phone_number) - len(phone_number[-4:])):
+#         result += "*"
+#         # 번호 뒷 4자리만 남기자
+#     back_n += phone_number[-4:]
         
-    return result + back_n
+#     return result + back_n
+
+def solution(phone_number): 
+    p_dict = {} # 빈 딕셔너리 만들기 
+    result = "" # 문자열 담기 
+    # 앞번호 순회할때 키와 값 정해주기 
+    for i in phone_number[:-4]:
+        p_dict[i] = "*"
+        # print(p_dict)
+        # i = 0 -> * {"0": "*"}
+        # i = 1 -> * {"1": "*"}
+    for i in p_dict.keys(): # 0, 1, 3
+        # print(i)
+        result += p_dict[i] * phone_number[:-4].count(i)
+        
+                                                
+    return result + phone_number[-4:]
