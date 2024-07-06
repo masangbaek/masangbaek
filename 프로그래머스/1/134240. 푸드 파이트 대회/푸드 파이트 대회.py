4a54415d92841b13df9e207323b31aeeb461856f
Sangbaek <masangbaek@gmail.com>
2024-07-06 14:56:06 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,24 +1,24 @@
-def solution(food):
-    answer = ''
-    n_food = [i if i % 2 == 0 else i - 1 for i in food ]
-    # print(n_food)
-    n_str = ""
-    n_str1 = ''
-    n_str2 = ''
-    for index, i in enumerate(n_food[1:]):
-        # print(index, i)
-        # print(index, i)
-        # print(food[3]) 음식의 수 
-        n_str += str(index + 1) * (i // 2) 
-        # print(n_str)
-        n_str1 = "0"
+def solution(food): # food 의 인덱스 : 음식종류, 원소 : 음식갯수
+    result = '' # 결과가 문자열이다.
+    # 1. "122333" 구현하기
+    # 한사람 먹을 분량으로 나눠주자
+    n_food = [i // 2 for i in food]
+    result1 = ''
         
-        n_str2 = n_str[::-1]
-    # print(n_str + n_str1 + n_str2)
-    # print(n_str2)
-    return n_str + n_str1 + n_str2
-           
-    # 물은 배열 중앙 위치 인덱스 0 값은 1 
-    # 1번 음식 양쪽 끝
-    # 인원 수 : 2먕 
+    for index, i in enumerate(n_food[1:]): # 인덱스 0은 물이니까 필요없음
+        # print(index, i) # 0 1 / 1 3 / 2 4 / 3 6 -> 0 3 / 1 4 / 2 6
+        # enumerate는 인덱스 0부터 시작한다.
+        # 1 3 / 2 4 / 2 6 을 구현해야한다
+        
+        # 문자열로 바꾸자 / 인덱스 + 1 을 해서 1부터 나오게 하자
+        result += str(index + 1) * i
+        # print(result) 
+        
+        # 2. 0 더해서 연결하기
+        
+        # 3. 1번 뒤집기
 
+    result1 += result[::-1]
+        
+    
+    return result + "0" + result1
