bd883970d842751e1bb47ffa35dffd0d2ceed927
Sangbaek <masangbaek@gmail.com>
2024-07-06 15:50:00 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,11 +1,11 @@
-def solution(phone_number):
-    result = ''
-    # print(type(phone_number))
-    for i in range(len(phone_number) - len(phone_number[-4:])): # 전체 길이 
-        result += "*" 
-    
-    # 뒷 4자리 숫자 
-    result += phone_number[-4:]
-        # print(+phone_number[-4:])
-    # print(len(phone_number) - len(phone_number[-4:]))
-    return result
+def solution(phone_number): 
+    # 전화번호 뒷 4자리를 제외한 나머지 숫자는 *이 되어야함 
+    result = '' # 문자열로 반환
+    back_n = ''
+    # 앞번호 *로 만들기 
+    for i in range(len(phone_number) - len(phone_number[-4:])):
+        result += "*"
+        # 번호 뒷 4자리만 남기자
+    back_n += phone_number[-4:]
+        
+    return result + back_n
