4743b7d51690e1b2ab62dc2267f18cf08b84ea8f
Sangbaek <masangbaek@gmail.com>
2024-08-21 08:09:09 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,7 +1,14 @@
+#     # 소문자라면 대문자로 변환
+#     # 대문자라면 소문자로 변환
+# def solution(my_string):
+#     answer = my_string.swapcase()
+#     return answer
+
 def solution(my_string):
-    answer = my_string.swapcase()
-    # 소문자라면 대문자로 변환
-    
-    
-    # 대문자라면 소문자로 변환
+    answer = ''
+    for s in my_string:
+        if 'a'<=s<='z':
+            answer += s.upper()
+        else:
+            answer += s.lower()
     return answer
