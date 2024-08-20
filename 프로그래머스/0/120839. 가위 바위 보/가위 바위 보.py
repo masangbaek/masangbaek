8140351921b01262559337a080bbd0148ec572a0
Sangbaek <masangbaek@gmail.com>
2024-08-21 08:26:15 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,6 +1,12 @@
 def solution(rsp):
-    d = {'0':'5','2':'0','5':'2'}
-    result = ''
-    for i in rsp :
-        result += d[i]
-    return result 
+    answer = ''
+    for s in rsp:
+        # print(s)
+        if s == '2':
+            answer += '0'
+            # print(s)
+        elif s == '0':
+            answer += '5'
+        else:
+            answer += '2'
+    return answer
