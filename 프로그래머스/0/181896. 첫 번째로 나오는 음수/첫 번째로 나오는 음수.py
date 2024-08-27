bc6ea716bd4d718ea42f42c0391b53e367d9c959
Sangbaek <masangbaek@gmail.com>
2024-08-27 10:18:59 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,9 +1,18 @@
-def solution(num_list) :
-    answer = 0
-    for i in range(len(num_list)):
-        #print(i)
-        if num_list[i] < 0:
-            #print(i) -2
-            return i
-    else:
-        return -1
+# 1
+# def solution(num_list) :
+#     answer = 0
+#     for i in range(len(num_list)):
+#         #print(i)
+#         if num_list[i] < 0:
+#             #print(i) -2
+#             return i
+#     else:
+#         return -1
+
+def solution(num_list):
+    for idx, num in enumerate(num_list):
+        #print(i, num) 0 12 / 1 4 / 2 15 ...
+        if num < 0:
+            return idx
+        
+    return -1
