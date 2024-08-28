1ab671444966dec09defd1b174b3403c5a17e47a
Sangbaek <masangbaek@gmail.com>
2024-08-29 08:34:47 +0900

    [level 0] Title: 카운트 다운, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
@@ -1,10 +1,14 @@
+# def solution(start_num, end_num):
+#     answer = []
+#     # [start_num : end_num : -1]
+#     for i in range(end_num, start_num + 1):
+#         # print(i) 3 4 5 6 7 8 9 10
+#         answer.append(i)      
+#     answer.sort(reverse=True)
+#     return answer
+
 def solution(start_num, end_num):
-    answer = []
-    # [start_num : end_num : -1]
-    for i in range(end_num, start_num + 1):
-        # print(i) 3 4 5 6 7 8 9 10
-        answer.append(i)      
-    answer.sort(reverse=True)
-    return answer
+    return list(range(start_num, end_num - 1, -1))  
+
     
   
