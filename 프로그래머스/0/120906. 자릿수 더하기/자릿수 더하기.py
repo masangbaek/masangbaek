77ed09c77a61b787e360483ca0d808100dea45db
Sangbaek <masangbaek@gmail.com>
2024-08-29 08:12:10 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.py
@@ -1,5 +1,13 @@
+# def solution(n):
+#     answer = []
+#     answer += str(n)
+#     # print(answer) ["1","2","3","4"]
+#     return sum(list(map(int, answer)))
+
 def solution(n):
-    answer = []
-    answer += str(n)
-    # print(answer) ["1","2","3","4"]
-    return sum(list(map(int, answer)))
+    n = str(n)
+    answer = 0 
+    for i in n:
+        # print(i) 1 2 3 4
+        answer += int(i)
+    return answer
