08b1dc3cb19e5daaef9b8ea4561c21658b0bc19a
Sangbaek <masangbaek@gmail.com>
2024-09-04 10:57:31 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,18 +1,18 @@
 def solution(num_list, n):
-    # answer = [ + 1 for i in range(n) for j in range(n)]
-    # return answer
-    
-    # 2차원 배열로 바꿀 때 들어갈 원소의 수 
-    result = []
-    for i in range(len(num_list) // n): # 2일 경우 0부터 1까지 반복 
-        # print(i) # 0 1 2 3
-        new = []
-        for j in num_list:
-            # print(j) 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
-            new.append(j) # [1,2,3,4,5,6,7,8]
-            new = num_list[i * n : (i + 1) * n]
-        result.append(new) 
-    return result # [[1,2,3,4,5,6,7,8]]
+    answer = [num_list[i * n : (i + 1) * n] for i in range(len(num_list) // n)]
+    return answer
+
+    # # 2차원 배열로 바꿀 때 들어갈 원소의 수 
+    # result = []
+    # for i in range(len(num_list) // n): # 2일 경우 0부터 1까지 반복 
+    #     # print(i) # 0 1 2 3
+    #     new = []
+    #     for j in num_list:
+    #         # print(j) 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
+    #         new.append(j) # [1,2,3,4,5,6,7,8]
+    #         new = num_list[i * n : (i + 1) * n]
+    #     result.append(new) 
+    # return result # [[1,2,3,4,5,6,7,8]]
       
 
   
