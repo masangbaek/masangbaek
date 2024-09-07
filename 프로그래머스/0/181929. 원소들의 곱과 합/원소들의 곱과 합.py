21a7c1d1c68a1c36f44e27112fad156dcdc341dc
Sangbaek <masangbaek@gmail.com>
2024-09-07 21:05:39 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,19 +1,25 @@
+# def solution(num_list):
+#     mul = 1
+#     plus = 0
+#     for i in num_list:
+#         # print(i) 3 4 5 2 1
+#         mul *= i
+#         plus += i
+#     # print(mul)
+#     # print(plus**2)
+#     if mul > plus**2:
+#         return 0
+#     else: 
+#         return 1
+    
 def solution(num_list):
-    mul = 1
-    plus = 0
+    mul = 1    
     for i in num_list:
         # print(i) 3 4 5 2 1
         mul *= i
-        plus += i
-    print(mul)
-    print(plus**2)
-    if mul > plus**2:
-        return 0
-    else: 
+    if (sum(num_list))**2 > mul:
+        print((sum(num_list))**2)
+        print(mul)
         return 1
-        # if (sum(num_list))**2 > mul:
-        #     print((sum(num_list))**2)
-        #     print(mul)
-        #     return 1
-        # else:
-        #     return 0 
+    else:
+        return 0 
