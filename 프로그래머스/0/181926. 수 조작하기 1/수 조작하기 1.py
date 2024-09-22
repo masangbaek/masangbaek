95da88ba8871e6c72186061aadecdc41fdfe38c3
Sangbaek <masangbaek@gmail.com>
2024-09-23 08:09:23 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.68 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,31 +1,28 @@
+# def solution(n, control):
+#     result = n
+    
+#     for i in control:
+#         if i == "w":
+#             result += 1
+#         elif i == "s":
+#             result -= 1
+#         elif i == "d":
+#             result += 10 
+#         elif i == "a":
+#             result -= 10 
+#     return result 
+    
 def solution(n, control):
     result = n
-    
+    # 0 → 1 → 0 → 10 → 0 → 1 → 0 → 10 → 0 → -1 → -2 → -1
+    dicts = {
+        "w" : 1,
+        "s" : -1,
+        "d" : 10,
+        "a" : -10
+    }
     for i in control:
-        if i == "w":
-            result += 1
-        elif i == "s":
-            result -= 1
-        elif i == "d":
-            result += 10 
-        elif i == "a":
-            result -= 10 
-    return result 
-    
-    
-
-# def solution(n, control):
-#     result = ''
-#     # 0 → 1 → 0 → 10 → 0 → 1 → 0 → 10 → 0 → -1 → -2 → -1
-#     dict = {
-#         "w" : n + 1,
-#         "s" : n - 1,
-#         "d" : n + 10,
-#         "a" : n - 10
-#     }
-#     for i in control:
-#         # print(i) w s d a w s d a s s w 
-#         if i in dict:
-#             result += dict[i]
-#     return result
+        # print(i) w s d a w s d a s s w 
+        result += dicts[i]
+    return result
         
